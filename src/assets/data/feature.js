6aa636e96aced41b17251a71cb6f4b0cde49f6d7
Akansh <akansh.gulati@wingify.com>
2018-03-29 01:35:06 +0530

    Fix(features-info): Improved the textual part

--- src/assets/data/feature.js
+++ src/assets/data/feature.js
@@ -1,30 +1,30 @@
 export const wallpapers = {
-  desc: 'Gives you refreshing wallpapers of your choice from different categories or you can custom wallpapers which changes after certain new tab. It makes sure you see a new wallpaper every.',
+  desc: 'Choose from 4 different categories - Nature, Architecture, Travel and Night life or make your own custom list of wallpapers.',
   heading: 'Refreshing Wallpapers',
   img: 'wallpapers.jpg',
   id: 'wallpaper'
 }
 export const notes = {
-  desc: 'No more visiting different websites to create handy notes, its available on every new tab. You can create different note lists and can add rich text in notes, support bold, italics, colored text, etc. by keyboard shortcuts.',
-  heading: 'Notes',
+  desc: 'Got some ideas running through your mind? Jot them down before you forget. It has multi-list and rich-text support.',
+  heading: 'Notes Widget',
   img: 'notes.jpg',
   id: 'notes'
 }
 export const weather = {
-  desc: 'Get updates on current weather condition, pollution level, forecast for next five days, etc. based on your current or custom location',
-  heading: 'Weather',
+  desc: 'Auto locates your location or supports custom location to stay informed about current weather, pollution and weather forecast',
+  heading: 'Weather Widget',
   img: 'weather.jpg',
   id: 'weather'
 }
 export const dateTime = {
-  desc: 'Keep track of current day and time',
-  heading: 'Date & Time',
+  desc: 'Keep track of time during the day to manage your tasks and improve productivity.',
+  heading: 'Date & Time Widget',
   img: 'day-time.jpg',
   id: 'dateTime'
 }
 export const customize = {
-  desc: 'Different customizations for widgets to suit your requirements. You can turn off a widget, set change interval of wallpapers, add custom wallpapers, change settings specific to widgets, etc.',
-  heading: 'Customize',
+  desc: 'Customize and show-hide features as per your needs.',
+  heading: 'Customizable',
   img: 'customize.jpg',
   id: 'customize'
 }
