d27f8c74e49d79997138f3dfb0eebe4d567961ac
Akansh Gulati <akanshgulati2009@gmail.com>
2020-04-29 01:11:16 +0530

    fix(PrivacyPolicy): Add coronatrends and akansh related information on privacy policy

--- src/components/Privacy.vue
+++ src/components/Privacy.vue
@@ -1,30 +1,464 @@
 <template>
   <section id="privacy" class="pv-20">
-    <h2>Privacy Policy</h2>
+    <h2>Privacy Notice</h2>
+    <p>
+      Subtletab, Inc (collectively “Subtletab”, "we", "us", and "ours") is
+      committed to protecting your privacy. This Privacy Notice (“Notice”)
+      describes how we processes Personal Data in its capacity as a controller
+      (i.e. Subtletab decides what Personal Data is collected and what it is
+      used for) or processor (i.e. Subtletab only processes the data as per the
+      controller's instructions), as the case may be. This policy is applicable
+      to Subtletab public website,
+      <a href="https://subtletab.com" title="subtletab">subtletab.com</a> and
+      Subtletab products' website and products itself (web platform, browser
+      extensions and mobile applications) such as
+      <a href="https://coronatrends.live" title="coronatrends"
+        >coronatrends.live</a
+      >
+      and
+      <a href="https://akansh.com" title="akansh">https://akansh.com</a>
+      website. In following notice, we will be referring to extensions or
+      add-ons or utilities which are only related to Chrome and Firefox
+      browsers.
+    </p>
+    <h3>Whom does this notice applies to?</h3>
+    <p>
+      This Notice applies to (i) entities or individuals, excluding End-Users
+      and Users, who have subscribed to our Service(s) and have agreed to the
+      permissions (hereinafter referred to as the “Customer”) and (ii)
+      individuals (“Individuals”) from whom Subtletab collects data as a
+      controller.
+    </p>
+    <h3>What Personal Information is collected and why?</h3>
+    When you visit Subtletab or its products' website, we may collect your
+    anonymous aggregated event related data (collectively called as "Collected
+    Data") using standard third party analytics service (Google Analytics) to
+    better understand and improve content and experience. No personal
+    information related to user is collected as a controller. For the purposes
+    of General Data Protection Regulation (GDPR), Subtletab shall be the
+    controller for the Collected Data – this means that Subtletab decides what
+    Collected Data is processed and why.
+    <strong
+      >Any information isn’t and has never been sold to third parties.</strong
+    >
 
-    <p>We are committed to protecting your privacy. This privacy policy applies to both our plugin/extension/add-on (Subtle Tab) and our website (Subtletab.com).</p>
-    <h3>Information Collection and Usage</h3>
-    <p>We don't collect any email or user specific information while installation or during usage of our product. We only collect anonymous aggregated event related data using an industry standard third party analytics service (Google Analytics) to better understand and improve the user behaviour on our product. The information we collect via third party services does not contain personal information like IP address, name, email address, etc. The information we collect is not shared with or sold to any third party.</p>
+    <h4>Data accessible through WebExtensions API</h4>
+    WebExtension APIs used within Chrome Extensions and Firefox Add-ons have
+    fine-grained permission levels that are enforced by the Web Browser,
+    restricting information that our extension has access to within your
+    Browser. The Subtletab and its other extensions can only access specific
+    information that you have explicitly granted permission for. We can not and
+    do not track your browsing history. We make sure to ask for optional
+    permissions specific to feature within extensions. When you requests an
+    optional permission, your Web Browser will make it show a clear dialog box
+    specifying what permission(s) are being requested. The feature will be
+    accessible only once you choose to allow the requested permission(s). Here
+    are the list of permissions as per extensions we are providing:
 
-    <h3>Your data</h3>
-    <p>Subtletab uses third party vendors and hosting partners to provide the necessary hardware, software, networking, storage, and related technology required to run it. Although Subtletab owns the code, and all rights to the Subtletab product, you retain all rights to your data.</p>
-    <h3>Security</h3>
-    <p>We make security a priority across the lifecycle. All communication with our servers is secure, using SSL/TLS (HTTPS and HSTS).</p>
+    <h5>
+      Permissions accessed by Subtletab
+      <a
+        href="https://chrome.google.com/webstore/detail/subtle-tab/ngigalmiikbffkcabedaikboeodibhga"
+        >Chrome</a
+      >
+      &
+      <a href="https://addons.mozilla.org/en-US/firefox/addon/subtletab/"
+        >Firefox</a
+      >
+      extension
+    </h5>
+    <table>
+      <thead>
+        <tr>
+          <th>Information/Permission</th>
+          <th>Feature</th>
+          <th>Detailed Usage</th>
+          <th>Optional</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Location</td>
+          <td>Weather & Forecast</td>
+          <td>
+            Location coordinates could be sent anonymously to our weather
+            provider API, i.e. Yahoo Weather to determine a nearby station which
+            can give precise information related to weather. We use our servers
+            as a mediator to make sure no personal information of user's IP is
+            shared. This information is never saved in Subtletab servers at any
+            point
+          </td>
+          <td>No</td>
+        </tr>
+        <tr>
+          <td>Storage</td>
+          <td>User Settings</td>
+          <td>
+            We use various storage types, i.e. Browser storage API, session
+            related storage and persisted storage, in order to save user
+            settings within the Extension while using the product. It also makes
+            sure that we don't ask same information from the user again while
+            using the product.
+          </td>
+          <td>No</td>
+        </tr>
+        <tr>
+          <td>Host Permission</td>
+          <td>Fetch Remote Information</td>
+          <td>
+            We request data from our own API, i.e.
+            <strong>api.subtletab.com</strong> related to wallpapers list,
+            weather information
+          </td>
+          <td>No</td>
+        </tr>
+        <tr>
+          <td>Google API</td>
+          <td>Fetch Calendar Events</td>
+          <td>
+            We request data from Google's Calendar API, i.e.
+            <strong>https://www.googleapis.com/</strong> once you add Access
+            Token in the customize menu. In the Google Permission dialog box,
+            you can clearly check that we take read-level permission. Once you
+            add Access Token in browser extension, all calendar related data is
+            fetched directly from Google servers to your local system/machine
+            and we have no access to any of your personal data.
+          </td>
+          <td>Yes</td>
+        </tr>
+        <tr>
+          <td>Wunderlist API</td>
+          <td>Fetch Todo</td>
+          <td>
+            We request data from Wunderlist API, i.e.
+            <strong>https://*.wunderlist.com/</strong> once you add Access Token
+            in the customize menu for Todo. In the Wunderlist Permission dialog
+            box, you can clearly check that we take read-level permission to
+            read and write to-do only. Once you add Access Token in browser
+            extension, all to-do related data is fetched directly from
+            Wunderlist servers to your local system/machine and we have no
+            access to any of your personal data.
+          </td>
+          <td>Yes</td>
+        </tr>
+        <tr>
+          <td>Todoist API</td>
+          <td>Fetch Todo</td>
+          <td>
+            We request data from Wunderlist API, i.e.
+            <strong>https://*.todoist.com/</strong> once you add Access Token in
+            the customize menu for Todo. In the Todoist Permission dialog box,
+            you can clearly check that we take read-level permission to read and
+            write to-do only. Once you add Access Token in extension, all to-do
+            related data is fetched directly from Todoist servers to your local
+            system/machine and we have no access to any of your personal data.
+          </td>
+          <td>Yes</td>
+        </tr>
+      </tbody>
+    </table>
+    You can prevent your location and/or IP address from being sent to directly
+    to our third-party weather provider by turning off the Weather app
+    (Customize → General → Show/Hide Weather) or alternatively, selecting custom
+    location (Customize → Weather → Custom Weather).
 
-    <h3>Changes</h3>
-    <p>Although most changes are likely to be minor, Subtletab may change its Privacy Policy from time to time, and in Subtletab’s sole discretion. Subtletab encourages visitors to frequently check this page for any changes to its Privacy Policy. Your continued use of this site after any change in this Privacy Policy will constitute your acceptance of such change.</p>
-    <p>If you have any questions, please <a href="mailto:contact@subtletab.com" title="Email Support">contact us</a>.</p>
+    <h5 id="coronatrends">
+      Permissions accessed by Covid-19 Tracker
+      <a
+        href="https://chrome.google.com/webstore/detail/covid-19-tracker/ehpkefnpbbmohfkanokkjklihkjbamjm"
+        >Chrome</a
+      >
+      &
+      <a href="https://addons.mozilla.org/en-US/firefox/addon/covid-19-tracker/"
+        >Firefox</a
+      >
+      extension
+    </h5>
+    <table>
+      <thead>
+        <tr>
+          <th>Information/Permission</th>
+          <th>Feature</th>
+          <th>Detailed Usage</th>
+          <th>Optional</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Alarm</td>
+          <td>Stat Sync</td>
+          <td>
+            Alarm, as the name conveys, is used as a scheduler to check
+            statistics of the selected region(s) by you in the background to
+            show increase in global cases notifications in menubar and show
+            updated cases in popup menu. We sync stats after every 10 minutes.
+          </td>
+          <td>No</td>
+        </tr>
+        <tr>
+          <td>Storage</td>
+          <td>User Settings</td>
+          <td>
+            We use various storage types, i.e. Browser storage API and persisted
+            storage, in order to save user last visited view and current stats
+            within the
+            <strong>Extension</strong> while using the product. It improves user
+            experience with the extension.
+          </td>
+          <td>No</td>
+        </tr>
+        <tr>
+          <td>Host Permission</td>
+          <td>Fetch Stats</td>
+          <td>
+            We request data from our own API, i.e.
+            <strong>api.coronatrends.live</strong> related to infected cases
+            globally and for the regions selected by user.
+          </td>
+          <td>No</td>
+        </tr>
+      </tbody>
+    </table>
+    <h3>What third party vendors & sub-processors do we use?</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>Vendor</th>
+          <th>Purpose</th>
+          <th style="min-width: 150px;">Product/Website</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Amazon Web Services, Inc.</td>
+          <td>
+            We are using AWS services to run our Cloud infrastructure (providing
+            the necessary hardware, software, networking, storage, and other
+            related technology required to run Subtletab and it's products).
+          </td>
+          <td>
+            Subtletab<br />
+            Covid-19 Tracker
+          </td>
+        </tr>
+        <tr>
+          <td>Flickr</td>
+          <td>
+            Most of the Subtletab and it's products photos and backgrounds are
+            hosted remotely on Flickr, A Yahoo Product. We leverage their CDN
+            service to deliver high quality and heavy photos without taking any
+            personal information of the user.
+          </td>
+          <td>
+            Subtletab
+          </td>
+        </tr>
+        <tr>
+          <td>Google Analytics</td>
+          <td>
+            Google Analytics, a product of Google Inc, is an industry standard
+            third-party analytics service that we use to improve understanding
+            of the usage and performance of our products and service(s). All
+            information sent directly to GA from your system and our servers is
+            anonymized before being sent.
+          </td>
+          <td>
+            Subtletab<br />
+            Akansh.com
+          </td>
+        </tr>
+        <tr>
+          <td>DuckDuckGo</td>
+          <td>
+            We are leveraging their API for showing icons of the Bookmarks in
+            Subtletab extension.
+          </td>
+          <td>
+            Subtletab
+          </td>
+        </tr>
+        <tr>
+          <td>Zoho</td>
+          <td>
+            Our email services and support is hosted on Zoho Corporations'
+            Office Suite. All mails received and sent are done using Zoho
+            services which are accessible on web and phone apps by Zoho.
+          </td>
+          <td>
+            Subtletab <br />
+            Covid-19 Tracker <br/>
+            Akansh.com
+          </td>
+        </tr>
+        <tr>
+          <td>Cloudflare, Inc</td>
+          <td>
+            We are using their service to prevent DDoS mitigation, Content
+            Delivery Network for website and DNS management.
+          </td>
+          <td>
+            Subtletab <br />
+            Covid-19 Tracker <br />
+            Akansh.com
+          </td>
+        </tr>
+        <tr>
+          <td>Netlify</td>
+          <td>
+            We are using Netlify to host static websites
+          </td>
+          <td>
+            Akansh.com
+          </td>
+        </tr>
+        <tr>
+          <td>Github</td>
+          <td>
+            We are using
+            <a href="https://github.com/akanshgulati" title="Github">Github</a>
+            to host all our private and public code in multiple repositories.
+          </td>
+          <td>
+            Subtletab, <br>Covid-19 Tracker<br>Akansh.com
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <h3>What are your rights in relation to your personal data?</h3>
+    <p>
+      Subtletab and it's products uses third party vendors and hosting partners
+      to provide the necessary hardware, software, networking, storage, and
+      related technology required to run it as mentioned above. Although we owns
+      the code, and all rights to the Subtletab product, you retain all rights
+      to your data.
+    </p>
+    <h5>The right to refuse Analytics data collection</h5>
+    The voluntary Personal information you provide to us is an integral part of
+    your use of Subtletab and its products. You can choose not to provide the
+    data, but that keep you restricted from using our services and products.
+    <h5>The right to remove all your information</h5>
+    <p>
+      Once you uninstall our products, we make sure that none of your personal
+      information persist in our servers. We may voluntarily ask you for filling
+      form on uninstall improve experience for all users.
+    </p>
+    <h5>The right to submit a complaint</h5>
+    <p>
+      If you have a complaint about the way in which your Personal Data is
+      handled, please
+      <a href="mailto:contact@subtletab.com" title="Email Support"
+        >contact us</a
+      >
+      or <a href="https://twitter.com/subtletab">tweet up</a> . After you submit
+      a complaint, we will acknowledge your complaint within 24 hour, will
+      investigate it and provide you respective response with two (1) week time.
+    </p>
+    <h5>The right to submit a complaint with a data protection authority</h5>
+    <p>
+      If you are a resident of the European Union coming under GDPR policy, and
+      you are not satisfied with the outcome of the complaint submitted to us,
+      you have the right to lodge a complaint with your local data protection
+      authority.
+    </p>
+    <h3>What about data security your servers?</h3>
+    <p>
+      We make security a priority across all the life cycles. All communications
+      to and fro our servers is secured using SSL/TLS making data transmission
+      secure. All Subtletab and its products are having HTTP String Transport
+      Policy. In our servers, all access logs and error logs are having no
+      personal information and backups are encrypted.
+    </p>
+    <h3>Will the privacy notice change?</h3>
+    <p>
+      Although most changes are likely to be minor, we may change our Privacy
+      Notice from time to time, and in Subtletab’s sole discretion. Subtletab
+      encourages visitors to frequently check this page for any changes to its
+      Privacy Policy. Your continued use of this site after any change in this
+      Privacy Policy will constitute your acceptance of such change.
+    </p>
+    <p>
+      If you have any questions, please
+      <a href="mailto:contact@subtletab.com" title="Email Support">contact us</a
+      >.
+    </p>
+    <div class="last-updated">Last updated: April 28, 2020.</div>
   </section>
 </template>
 <script>
-  export default {
-
-  }
+export default {};
 </script>
 <style>
-  #privacy {
-    max-width: 1220px;
-    width: 75%;
-    margin: 0 auto;
-  }
+#privacy {
+  max-width: 1000px;
+  width: 90%;
+  margin: 0 auto;
+  line-height: 2;
+  color: #333;
+}
+#privacy * {
+  color: #333;
+}
+
+#privacy a {
+  text-decoration: underline;
+}
+
+section#privacy h2 {
+  text-align: center;
+  font-size: 36px;
+  line-height: 1.4em;
+}
+
+section h3 {
+  font-size: 2rem;
+  line-height: 1.1;
+}
+
+section h4 {
+  font-size: 1.5rem;
+  line-height: 1.1;
+}
+
+section h5,
+section h5 a {
+  font-size: 1.2rem;
+  line-height: 1;
+  font-weight: 600;
+}
+
+section strong {
+  font-weight: 600;
+}
+
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  margin-bottom: 2.5em;
+}
+
+table th {
+  opacity: 0.7;
+  font-size: 0.9rem;
+  font-weight: 600;
+  text-align: left;
+  text-transform: uppercase;
+}
+
+table th,
+table td {
+  padding: 14px 12px;
+}
+
+tbody tr:nth-child(even) {
+  background: rgba(0, 0, 0, 0.04);
+}
+td {
+  border-top: 1px solid rgba(0, 0, 0, 0.2);
+  font-size: 0.9rem;
+  line-height: 1.2;
+  vertical-align: top;
+}
+.last-updated {
+  color: #666;
+}
 </style>
